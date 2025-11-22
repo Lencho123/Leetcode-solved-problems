2d3d73e0404844c8d3d17442f1e1b122e6a6eb15
Lencho123 <lencholachisa11@gmail.com>
2025-11-22 19:57:38 +0300

    Time: 559 ms (64.66%), Space: 18 MB (80.57%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -1,16 +1,39 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        target = sum(nums)/2
-        sums = set()
-        
-        for i in range(len(nums)):
-            cur_sums = set([])|sums
-            for s in cur_sums:
-                if target >= s+nums[i]:
-                    sums.add(s+nums[i])
-            sums.add(nums[i])
+        # BOTTOM UP SPACE EFFICIENT
+        total = sum(nums)
+        if total % 2 != 0:
+            return False
+        target = total // 2
 
-        if target in sums:
-            return True
+        dp = [False] * (target+1)
+        dp[0] = True
 
-        return False
+        for num in nums:
+            for j in range(target, num-1, -1):
+                dp[j] = dp[j] or dp[j-num]
+
+        return dp[target]
+
+
+        # ###  TOP DOWN SOLUTION
+        # dp = {}
+        # target = sum(nums)/2
+        # def canSumUpToHalf(ind,leftSum):
+        #     if leftSum == 0:
+        #         return True
+        #     if leftSum < 0 or ind >= len(nums):
+        #         return False
+        #     if (ind,leftSum) in dp:
+        #         return dp[(ind,leftSum)]
+            
+        #     temp = False
+        #     # Take from ind
+        #     temp|= canSumUpToHalf(ind+1,leftSum-nums[ind])
+        #     # Not take from ind
+        #     temp|= canSumUpToHalf(ind+1, leftSum)
+        #     dp[(ind,leftSum)] = temp
+
+        #     return temp
+
+        return canSumUpToHalf(0,target)
