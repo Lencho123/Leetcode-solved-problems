0aa5adf25a72cac89ddad35c354899cdfd0fca39
Lencho123 <lencholachisa11@gmail.com>
2025-11-23 13:09:38 +0300

    Time: 1539 ms (9.28%), Space: 309.9 MB (13.71%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -1,19 +1,38 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        dp = [[0]*len(text1) for i in range(len(text2))]
+        # ====TOP DODWN======#
+        cache = {}
+        def dfs(i,j):
+            if i == len(text1) or j == len(text2):
+                return 0
+            
+            if (i,j) in cache:
+                return cache[(i,j)]
+            
+            if text1[i] == text2[j]:
+                cache[(i,j)] = 1+dfs(i+1,j+1)
+            else:
+                cache[(i,j)] =  max(dfs(i+1,j), dfs(i,j+1))
+            return cache[(i,j)]
+            
+        return dfs(0,0)
 
-        def in_bound(r,c):
-            return 0 <= r < len(text2) and 0 <= c < len(text1)
+
+        # ====BOTTOM UP======#
+        # dp = [[0]*len(text1) for i in range(len(text2))]
+
+        # def in_bound(r,c):
+        #     return 0 <= r < len(text2) and 0 <= c < len(text1)
 
 
-        for i in range(len(text2)):
-            for j in range(len(text1)):
-                equal = text1[j] == text2[i]
-                left_val = dp[i][j-1] if in_bound(i,j-1) else 0
-                top_val = dp[i-1][j] if in_bound(i-1,j) else 0
-                top_left = dp[i-1][j-1] if in_bound(i-1,j-1) else 0
-                if equal:
-                    dp[i][j] = top_left + 1
-                else:
-                    dp[i][j] = max(left_val, top_val)
-        return dp[-1][-1]
+        # for i in range(len(text2)):
+        #     for j in range(len(text1)):
+        #         equal = text1[j] == text2[i]
+        #         left_val = dp[i][j-1] if in_bound(i,j-1) else 0
+        #         top_val = dp[i-1][j] if in_bound(i-1,j) else 0
+        #         top_left = dp[i-1][j-1] if in_bound(i-1,j-1) else 0
+        #         if equal:
+        #             dp[i][j] = top_left + 1
+        #         else:
+        #             dp[i][j] = max(left_val, top_val)
+        # return dp[-1][-1]
