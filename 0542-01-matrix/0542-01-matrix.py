e824fc6df93ca2b0cd18f73c5cef341efa3a0488
Lencho123 <lencholachisa11@gmail.com>
2025-12-10 13:54:28 +0300

    Time: 243 ms (14.07%), Space: 21.1 MB (30.55%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.py
+++ 0542-01-matrix/0542-01-matrix.py
@@ -1,44 +1,36 @@
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
-        m,n = len(mat),len(mat[0])
-
-        dp = []
-
+        row,col = len(mat),len(mat[0])
+        dirc = [[0,1],[1,0],[-1,0],[0,-1]]
+        
+        def in_bound(r,c):
+            return 0<=r<row and 0<=c<col
+        
+        
         visited = set()
         que = deque()
-
-        for i in range(m):
-            row = []
-            for j in range(n):
-                if mat[i][j]:
-                    row.append(float("inf"))
-                else:
-                    row.append(0)
-                    visited.add((i,j))
-                    que.append((i,j))   
         
-            dp.append(row)
-
+        for i in range(row):
+            for j in range(col):
+                if mat[i][j] == 0:
+                    visited.add((i,j))
+                    que.append((i,j))
+                    
+            
+        level = 0
         while que:
             l = len(que)
-
+            
             for i in range(l):
                 r,c = que.popleft()
-                val = dp[r][c]
-                if 0<=c-1<n and (r,c-1) not in visited:
-                    dp[r][c-1] = val+1
-                    visited.add((r,c-1))
-                    que.append((r,c-1))
-                if 0<=c+1<n and (r,c+1) not in visited:
-                    dp[r][c+1] = val+1
-                    visited.add((r,c+1))
-                    que.append((r,c+1))
-                if 0<=r-1<m and (r-1,c) not in visited:
-                    visited.add((r-1,c))
-                    dp[r-1][c] = val+1
-                    que.append((r-1,c))
-                if 0<=r+1<m and (r+1,c) not in visited:
-                    visited.add((r+1,c))
-                    dp[r+1][c] = val+1
-                    que.append((r+1,c))
-        return dp
+                mat[r][c] = level
+                for a,b in dirc:
+                    if in_bound(r+a,c+b):
+                        if (r+a,c+b) not in visited:
+                            que.append((r+a,c+b))
+                            visited.add((r+a,c+b))
+                            
+            level+=1
+        return mat
+                            
+                            
