3e4e7dd93881e052f93e1e8af928d982af90a5e1
Lencho123 <lencholachisa11@gmail.com>
2025-12-13 15:58:52 +0300

    Time: 0 ms (100%), Space: 18 MB (29.62%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -1,20 +1,21 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        if s[0] == "0":
-            return 0
-
-        dp = defaultdict(int)
-        def dfs(i):
-            if i == len(s):
-                return 1 
-            
-            if s[i] == "0":
-                return 0
-
-            if (i+1,i+1) not in dp:
-                dp[(i+1,i+1)] = dfs(i+1)
-            if i < len(s) - 1 and "10" <= s[i:i+2] <= "26" and (i+1,i+2) not in dp:
-                dp[(i+1,i+2)] = dfs(i+2)
-            return dp[(i+1,i+1)] + dp[(i+1,i+2)]
+        dp = {}
         
+        def dfs(ind):
+            if ind == len(s):
+                return 1
+            if s[ind] == "0":
+                return 0
+            if ind in dp:
+                return dp[ind]
+            temp = 0
+            #take one digit
+            temp+=dfs(ind+1)
+            #take two digit
+            if ind < len(s)-1 and int(s[ind:ind+2]) <= 26:
+                temp+=dfs(ind+2)
+            dp[ind] = temp
+            return dp[ind]
         return dfs(0)
+                
