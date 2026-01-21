595f30427b84b389b072ec6756762d2baf346379
Lencho123 <lencholachisa11@gmail.com>
2026-01-21 11:33:33 +0300

    Time: 7 ms (71.74%), Space: 19.4 MB (18.12%) - LeetHub

--- 3314-construct-the-minimum-bitwise-array-i/3314-construct-the-minimum-bitwise-array-i.py
+++ 3314-construct-the-minimum-bitwise-array-i/3314-construct-the-minimum-bitwise-array-i.py
@@ -3,11 +3,25 @@ class Solution:
         ans = []
 
         def find_min_bw(num):
-            for i in range(num):
-                if num == i|(i+1):
-                    return i
-            return -1
-        
+            if num == 2: return -1
+            flag = True
+            pos = 0
+            res = 0
+            while flag:
+                if not num & 1:
+                    flag = False
+                    res-=2**(pos-1)
+                else:
+                    res+=2**pos
+                    num>>=1
+                    pos+=1
+
+            while num:
+                res+=(num&1)*2**pos
+                num>>=1
+                pos+=1
+            return res
+            
         for n in nums:
             ans.append(find_min_bw(n))
         return ans
