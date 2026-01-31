d0c2f20e52f2bbae48390704e8b4488da88bb2ed
Lencho123 <lencholachisa11@gmail.com>
2026-02-01 02:26:39 +0300

    Time: 0 ms (100%), Space: 19.5 MB (9.6%) - LeetHub

--- 2148-count-elements-with-strictly-smaller-and-greater-elements/2148-count-elements-with-strictly-smaller-and-greater-elements.py
+++ 2148-count-elements-with-strictly-smaller-and-greater-elements/2148-count-elements-with-strictly-smaller-and-greater-elements.py
@@ -1,15 +1,25 @@
 class Solution:
     def countElements(self, nums: List[int]) -> int:
+        # SOLUTION-1 O(n^2)
+        # ans = 0
+        # for i in range(len(nums)):
+        #     greater,smaller = False,False
+        #     for j in range(len(nums)):
+        #         if nums[i] < nums[j]:
+        #             greater = True
+        #         if nums[i] > nums[j]:
+        #             smaller = True
+        #     if greater and smaller:
+        #         ans+=1
+        # return ans
+
+
+        #SOLUTION-2 O(n)
         ans = 0
+        min_val = min(nums)
+        max_val = max(nums)
 
         for i in range(len(nums)):
-            greater,smaller = False,False
-            for j in range(len(nums)):
-                if nums[i] < nums[j]:
-                    greater = True
-                if nums[i] > nums[j]:
-                    smaller = True
-            if greater and smaller:
+            if min_val < nums[i] < max_val:
                 ans+=1
-                
         return ans
